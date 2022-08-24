689c7eedf5692eb43b6eb75ac945e475abf013fd
Pål Anders Owren <palandersowren@Pals-MacBook-Pro-2.local>
2022-08-24 13:59:10 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('hei verden!'));
+app.get('/', (req, res) => res.send('monkey on the car'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
